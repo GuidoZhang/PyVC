d4c6388bf42236de78e57f3c8d8f8ea172b702d6
GuidoZhang <guidozhang@qq.com>
2017-07-15 13:41:27 +0800

    init

---
+++ PyVC/PyVC/urls.py
@@ -0,0 +1,55 @@
+﻿"""
+Definition of urls for PyVC.
+"""
+
+from datetime import datetime
+from django.conf.urls import url
+from app.forms import BootstrapAuthenticationForm
+import django
+from app import views
+import django.contrib.auth.views as djAuthView
+# Uncomment the next lines to enable the admin:
+from django.conf.urls import include
+from django.contrib import admin
+admin.autodiscover()
+
+urlpatterns = [
+    # Examples:
+    url(r'^$', views.home, name='home'),
+    url(r'^contact$', views.contact, name='contact'),
+    url(r'^about', views.about, name='about'),
+    url(r'^login/$',
+        djAuthView.login,
+        {
+            'template_name': 'app/login.html',
+            'authentication_form': BootstrapAuthenticationForm,
+            'extra_context':
+            {
+                'title':'Log in',
+                'year':datetime.now().year,
+            }
+        },
+        name='login'),
+    url(r'^logout$',
+        djAuthView.logout,
+        {
+            'next_page': '/',
+        },
+        name='logout'),
+
+    url(r'^profile$',
+        views.profile,
+        {},
+        name='profile'),
+
+    url(r'^profile/modify/password$',
+        views.modify_password,
+        {},
+        name='modify_password'),
+
+    # Uncomment the admin/doc line below to enable admin documentation:
+     url(r'^admin/doc/', include('django.contrib.admindocs.urls')),
+
+    # Uncomment the next line to enable the admin:
+     url(r'^admin/', include(admin.site.urls)),
+]
---
+++ PyVC/app/views.py
@@ -0,0 +1,64 @@
+﻿"""
+Definition of views.
+"""
+
+from django.shortcuts import render
+from django.http import HttpRequest
+from django.template import RequestContext
+from datetime import datetime
+from django.contrib.auth.decorators import login_required
+
+def home(request):
+    """Renders the home page."""
+    assert isinstance(request, HttpRequest)
+    return render(
+        request,
+        'app/home.html',
+        context = 
+        {
+            'title':'Home Page',
+            'year':datetime.now().year,
+        }
+    )
+
+@login_required
+def profile(request):
+    assert isinstance(request, HttpRequest)
+    return render(
+        request,
+        'app/user/profile.html',
+        )
+
+@login_required
+def modify_password(request):
+    assert isinstance(request, HttpRequest)
+    return render(
+        request,
+        'app/user/settings/password.html',
+        )
+
+def contact(request):
+    """Renders the contact page."""
+    assert isinstance(request, HttpRequest)
+    return render(
+        request,
+        'app/contact.html',
+        context = {
+            'title':'Contact',
+            'message':'Your contact page.',
+            'year':datetime.now().year,
+        }
+    )
+
+def about(request):
+    """Renders the about page."""
+    assert isinstance(request, HttpRequest)
+    return render(
+        request,
+        'app/about.html',
+        context = {
+            'title':'About',
+            'message':'Your application description page.',
+            'year':datetime.now().year,
+        }
+    )
